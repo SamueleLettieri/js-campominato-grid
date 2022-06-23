5c1f6ddcc931ad1bc27b4bc1f8ff690452d45923
SamueleLettieri <samuele2073@gmail.com>
2022-06-23 16:37:56 +0200

    griglia per il campo minato

---
+++ js/script.js
@@ -0,0 +1,31 @@
+const container = document.getElementById("contenitore");
+
+function newElementSquare (){
+    const newElement = document.createElement("div");
+    newElement.classList.add("square");
+    return newElement;
+};
+
+
+
+
+const play = document.getElementById("button");
+
+play.addEventListener('click', function(){
+    for (let i = 1; i <= 100; i++){
+        let newSquare = newElementSquare ();
+    
+        newSquare.addEventListener('click', function(){
+            newSquare.classList.add("active")
+            console.log(`hai cliccato ${i}`)
+        });
+    
+        newSquare.innerHTML = i
+    
+        container.append(newSquare);
+    };
+})
+
+
+
+
