b72872309f401ce37fa6909fd3d1e4557c2a01f7
SamueleLettieri <samuele2073@gmail.com>
2022-06-23 18:40:57 +0200

    griglia per il campo minato, con bonus

--- js/script.js
+++ js/script.js
@@ -2,21 +2,40 @@ const container = document.getElementById("contenitore");
 
 function newElementSquare (){
     const newElement = document.createElement("div");
-    newElement.classList.add("square");
+    newElement.classList.add("square_normale");
     return newElement;
 };
 
 
 
-
 const play = document.getElementById("button");
+const selec = document.getElementById("difficolta");
 
 play.addEventListener('click', function(){
-    container.innerHTML = null
-
-    for (let i = 1; i <= 100; i++){
-        let newSquare = newElementSquare ();
+    container.innerHTML = "";
+    let livello = selec.value;
+    let dimensione; 
+    let classe = newElementSquare ();
+    if(livello === "Normale"){
+        dimensione = 100; 
+    } else if( livello === "Difficile"){
+        dimensione = 81;
+    } else if(livello === "Ultra-difficile"){
+        dimensione = 49;
+    }
     
+    console.log(livello) 
+    for (let i = 1; i <= dimensione; i++){
+        let newSquare = newElementSquare ();
+        if(dimensione === 81){
+            newSquare.classList.remove("square_normale");
+            newSquare.classList.add("square_difficile");
+        }else if(dimensione === 49) {
+            newSquare.classList.remove("square_normale");
+            newSquare.classList.add("square_ultra_difficile");
+        }
+
+
         newSquare.addEventListener('click', function(){
             newSquare.classList.add("active")
             console.log(`hai cliccato ${i}`)
@@ -27,7 +46,6 @@ play.addEventListener('click', function(){
         container.append(newSquare);
     };
 })
-
 
 
 
